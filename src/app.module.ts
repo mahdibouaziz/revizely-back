afae880ac8f0a89fade7887b15134202ae2540ba
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-11-16 19:28:50 +0000

    added the connetion to the database (mongo) and basic register functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://scrum:scrum123@cluster0.sr2ks.mongodb.net/revizely?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  // Patch,
+  // Param,
+  // Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async register(@Body() user: User) {
+    console.log(user);
+    return await this.usersService.create(user);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
+}
